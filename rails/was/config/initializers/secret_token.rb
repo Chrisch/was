4ac2d12d8079b25583b320e450047a41235cb96c
Markus Hanses <markus@markushanses.de>
2011-01-11 20:56:17 +0100

    First step with structure for webapp

---
+++ rails/was/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Was::Application.config.secret_token = '612433310230ec93e51dc6ad258a10c8c74759b04138a4210f629b0cbf1a5dcfa82cba888978dc8df37540763c5088d484764443138ef60aff0aa9dc4d461a8d'
